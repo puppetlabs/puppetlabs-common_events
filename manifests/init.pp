2e6f27bd0e5cb45786856d6b0a370c0f08cf8cab
coreymbe <matibenefrayim@gmail.com>
2021-07-08 17:32:34 -0700

    (PIE-723) Moved scripts and support files via init.pp
    Created new file resources for required directories and copy scripts.
    Added require_relative to scripts to address change from gem.

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,13 +12,14 @@ class common_events (
 ) {
 
   # Account for the differences in Puppet Enterprise and open source
-  if $facts['common_events_is_pe'] {
+  if $facts[pe_server_version] != undef {
     $owner          = 'pe-puppet'
     $group          = 'pe-puppet'
   }
   else {
-    $owner          = 'puppet'
-    $group          = 'puppet'
+    notify { 'Non-PE':
+      message => "Error: This module is intended for use with Puppet Enterprise only.",
+    }
   }
 
   if (
@@ -37,32 +38,49 @@ class common_events (
   cron { 'collect_common_events':
     ensure  => 'present',
     command => @("COMMAND"/L),
-      ${settings::confdir}/collect_api_events.rb \
-      ${settings::confdir} \
+      ${settings::confdir}/common_events/collect_api_events.rb \
+      ${settings::confdir}/common_events \
       ${settings::modulepath} \
-      ${settings::statedir}
+      ${settings::confdir}/common_events/processors.d
       |-COMMAND
     user    => 'root',
     minute  => '*/2',
     require => [
-      File["${settings::confdir}/collect_api_events.rb"],
-      File["${settings::confdir}/events_collection.yaml"]
+      File["${settings::confdir}/common_events/collect_api_events.rb"],
+      File["${settings::confdir}/common_events/events_collection.yaml"]
     ],
   }
 
-  file { "${settings::confdir}/events_collection.yaml":
+  file { "${settings::confdir}/common_events":
+    ensure  => directory,
+    owner   => $owner,
+    group   => $group,
+    recurse => 'remote',
+    source  => 'puppet:///modules/common_events/lib',
+  }
+
+  file { "${settings::confdir}/common_events/processors.d":
+    ensure  => directory,
+    owner   => $owner,
+    group   => $group,
+    require => File["${settings::confdir}/common_events"],
+  }
+
+  file { "${settings::confdir}/common_events/events_collection.yaml":
     ensure  => file,
     owner   => $owner,
     group   => $group,
     mode    => '0640',
+    require => File["${settings::confdir}/common_events"],
     content => epp('common_events/events_collection.yaml'),
   }
 
-  file { "${settings::confdir}/collect_api_events.rb":
-    ensure => file,
-    owner  => $owner,
-    group  => $group,
-    mode   => '0755',
-    source => 'puppet:///modules/common_events/collect_api_events.rb',
+  file { "${settings::confdir}/common_events/collect_api_events.rb":
+    ensure  => file,
+    owner   => $owner,
+    group   => $group,
+    mode    => '0755',
+    require => File["${settings::confdir}/common_events"],
+    source  => 'puppet:///modules/common_events/collect_api_events.rb',
   }
 }
